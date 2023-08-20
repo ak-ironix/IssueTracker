df0815d40eecf1780235d45f7255a4234ead0f55
Akshat Upadhyay <akchatasr@gmail.com>
2023-08-20 20:04:39 +0530

    Added swagger ui and some final touches

--- server/controllers/issues.js
+++ server/controllers/issues.js
@@ -1,4 +1,4 @@
-const { issues } = require('../models')
+const { issues, newAuth } = require('../models')
 const jwt = require('jsonwebtoken')
 
 const getIssues = async (req, res, next) => {
@@ -23,15 +23,58 @@ const createIssue = async (req, res, next) => {
     }
 }
 
+const grantRight = async (req, res, next) => {
+    try {
+        const issueId = req.params.id;
+        const userId = req.user.id;
+        const newUserId = req.body.coauthor;
+        console.log(userId);
+        const thisIssue = await issues.findAll({
+            where: {
+                id: issueId,
+                userId: userId
+            }
+        })
+        if (thisIssue.length === 0) {
+            return res.status(400).json({ message: "Issue doesn't exist or not authorized to modify this issue" });
+        }
+        const newEntry = {
+            issueId: issueId,
+            userId: newUserId
+        }
+        await newAuth.create(newEntry)
+        return res.json(newEntry)
+
+    } catch (error) {
+        return res.status(500).json({ message: error })
+    }
+}
+
 const updateIssue = async (req, res, next) => {
     const id = req.params.id;
+    const userId = req.user.id;
+    const oAuthor = await issues.findAll({
+        where: {
+            id: id,
+            userId: userId
+        }
+    })
+    const coAuthor = await newAuth.findAll({
+        where: {
+            issueId: id,
+            userId: userId
+        }
+    })
+    if (oAuthor.length === 0 && coAuthor.length === 0) {
+        return res.status(400).json({ message: "Issue doesn't exist or not authorized to modify this issue" });
+    }
     try {
         await issues.update(
             { title: req.body.title, status: req.body.status },
             { where: { id: id } }
         )
     } catch (error) {
-        return res.send(400).json({ message: "invalid request" })
+        return res.status(400).json({ message: "Invalid request" })
     }
     return res.json({ message: "Inserted succesfully" })
 }
@@ -39,5 +82,6 @@ const updateIssue = async (req, res, next) => {
 module.exports = {
     createIssue,
     getIssues,
-    updateIssue
+    updateIssue,
+    grantRight
 }
--- server/routes/issues.js
+++ server/routes/issues.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { createIssue, getIssues, updateIssue } = require('../controllers/issues')
+const { createIssue, getIssues, updateIssue, grantRight } = require('../controllers/issues')
 const authMiddleware = require('../middleware/auth')
 
 router.get('/', authMiddleware, getIssues)
@@ -8,5 +8,7 @@ router.get('/', authMiddleware, getIssues)
 router.post('/', authMiddleware, createIssue)
 
 router.put('/:id', authMiddleware, updateIssue)
+
+router.post('/:id', authMiddleware, grantRight)
 
 module.exports = router
